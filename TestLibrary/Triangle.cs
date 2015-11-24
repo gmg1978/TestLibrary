246c95c1562efb9ae4809c829636e8d8544095d7
gmg1978 <688500@gmail.com>
2015-11-25 00:54:41 +0300

    Тестовое задание

---
+++ TestLibrary/Triangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    /// <summary>
+    /// Класс прямоугольного треугольника )
+    /// </summary>
+    public class Triangle
+    {
+
+        double A { get; set; }
+        double B { get; set; }
+        double C { get; set; }
+
+        /// <summary>
+        /// Создание треугольника
+        /// </summary>
+        /// <param name="a">Длина cтороны A</param>
+        /// <param name="b">Длина cтороны B</param>
+        /// <param name="c">Длина cтороны C</param>
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0)
+                throw new System.ArgumentException("Параметр не может быть отрицательным", "a");
+            if (b <= 0)
+                throw new System.ArgumentException("Параметр не может быть отрицательным", "b");
+            if (c <= 0)
+                throw new System.ArgumentException("Параметр не может быть отрицательным", "c");
+
+            A = a;
+            B = b;
+            C = c;
+        }
+
+
+
+        /// <summary>
+        /// Площадь прямоугольного треугольника
+        /// </summary>
+        public double Area
+        {
+            get
+            {
+                double semiPerimeter = Perimeter / 2;
+                return Math.Sqrt(semiPerimeter * (semiPerimeter - A) * (semiPerimeter - B) * (semiPerimeter - C));
+            }
+        }
+
+        /// <summary>
+        /// Периметр треугольника
+        /// </summary>
+        public double Perimeter
+        {
+            get
+            {
+                return A + B + C;
+            }
+        }
+
+
+    }
+}
---
+++ UnitTestTestLibrary/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibrary;
+
+namespace UnitTestTestLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TriangleTest()
+        {
+            // Create an instance to test:
+            Triangle _triangle = new Triangle(10,5,7);
+
+            // Define a test input and output value:
+            double expectedResult = 25;
+
+            // Run the method under test:
+            double actualResult = _triangle.Area;
+            
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+            //Assert.AreEqual(expectedResult, actualResult, 0.001, "Ошибка в расчете");
+        
+            
+        }
+    }
+}
